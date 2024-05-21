4e76937210741f3009cab257ea29fe48a27d21f2
KateMassa <catherineorlova27@gmail.com>
2024-05-21 14:47:13 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import { initMongoDB } from './db/initMongoDB.js';
+import { initMongoConnection } from './db/initMongoDB.js';
 import { setupServer } from './server.js';
 
 const bootstrap = async () => {
-  await initMongoDB();
+  await initMongoConnection();
   setupServer();
 };
 
