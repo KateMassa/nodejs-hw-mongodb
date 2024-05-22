d2e5946f5e137a0c886e09903768574758a8f1b1
KateMassa <catherineorlova27@gmail.com>
2024-05-22 13:55:26 +0200

    update settings

--- src/utils/env.js
+++ src/utils/env.js
@@ -3,7 +3,6 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export function env(name, defaultValue) {
-  // eslint-disable-next-line no-undef
   const value = process.env[name];
 
   if (value) return value;
