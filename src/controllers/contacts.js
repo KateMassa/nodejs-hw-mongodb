3c43449bc51bb54f23a00784b4bf00b239499c14
KateMassa <catherineorlova27@gmail.com>
2024-05-24 09:50:26 +0200

    Update contacts.js

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -18,10 +18,10 @@ export const getAllContactsController = async (req, res) => {
 
 export const getContactByIdController = async (req, res, next) => {
   const { contactId } = req.params;
-  if (!/^\d+$/.test(contactId)) {
-    next(createHttpError(400, 'Invalid contact ID'));
-    return;
-  }
+  // if (!/^\d+$/.test(contactId)) {
+  //   next(createHttpError(400, 'Invalid contact ID'));
+  //   return;
+  // }
   const contact = await getContactById(contactId);
   if (!contact) {
     next(createHttpError(404, 'Contact not found'));
@@ -35,11 +35,11 @@ export const getContactByIdController = async (req, res, next) => {
 };
 
 export const createContactController = async (req, res) => {
-  const newContact = await createContact(req.body);
+  const contact = await createContact(req.body);
   res.status(201).json({
     status: 201,
     message: 'Successfully created contact!',
-    data: newContact,
+    data: contact,
   });
 };
 
@@ -49,8 +49,8 @@ export const deleteContactController = async (req, res, next) => {
     next(createHttpError(400, 'Invalid contact ID'));
     return;
   }
-  const deletedContact = await deleteContact(contactId);
-  if (!deletedContact) {
+  const contact = await deleteContact(contactId);
+  if (!contact) {
     next(createHttpError(404, 'Contact not found'));
     return;
   }
@@ -63,17 +63,17 @@ export const updateContactController = async (req, res, next) => {
     next(createHttpError(400, 'Invalid contact ID'));
     return;
   }
-  const updatedContact = await updateContact(contactId, req.body, {
+  const result = await updateContact(contactId, req.body, {
     upsert: true,
   });
-  if (!updatedContact) {
+  if (!result) {
     next(createHttpError(404, 'Contact not found'));
     return;
   }
   res.status(200).json({
     status: 200,
     message: `Successfully upserted contact with id ${contactId}!`,
-    data: updatedContact.contact,
+    data: result.contact,
   });
 };
 
