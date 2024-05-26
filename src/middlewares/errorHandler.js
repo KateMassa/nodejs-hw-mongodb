25d31c98ea6340f327e49f8b724cad4b4dbf4591
KateMassa <catherineorlova27@gmail.com>
2024-05-26 11:50:06 +0200

    Update errorHandler.js

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,18 +1,18 @@
-// import { HttpError } from 'http-errors';
+import { HttpError } from 'http-errors';
 
 export const errorHandler = (err, req, res) => {
-  // if (err instanceof HttpError) {
-  //   res.status(err.status).json({
-  //     status: err.status,
-  //     message: err.name,
-  //     data: err,
-  //   });
-  //   return;
-  // }
+  if (err instanceof HttpError) {
+    res.status(err.status).json({
+      status: err.status,
+      message: err.name,
+      data: err,
+    });
+    return;
+  }
 
   res.status(500).json({
-    // status: 500,
+    status: 500,
     message: 'Something went wrong',
-    // data: err.message,
+    data: err.message,
   });
 };
