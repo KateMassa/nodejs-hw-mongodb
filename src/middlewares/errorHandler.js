908b8846f5c98259ffc6ecb2e1d870eb99f89a32
KateMassa <catherineorlova27@gmail.com>
2024-05-26 11:45:13 +0200

    Update errorHandler.js

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -11,8 +11,8 @@ export const errorHandler = (err, req, res) => {
   // }
 
   res.status(500).json({
-    status: 500,
+    // status: 500,
     message: 'Something went wrong',
-    data: err.message,
+    // data: err.message,
   });
 };
