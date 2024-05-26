be078fcf7e5b85d4eccac6cff11eceef132195c6
KateMassa <catherineorlova27@gmail.com>
2024-05-26 10:22:01 +0200

    ++

--- src/middlewares/notFoundHandler.js
+++ src/middlewares/notFoundHandler.js
@@ -1,17 +1,5 @@
-// import { HttpError } from 'http-errors';
-
-export const notFoundHandler = (err, req, res) => {
-  // if (err instanceof HttpError) {
-  //   res.status(err.status).json({
-  //     status: err.status,
-  //     message: err.name,
-  //     data: err,
-  //   });
-  //   return;
-  // }
+export const notFoundHandler = (req, res) => {
   res.status(404).json({
-    status: 404,
-    message: 'Contact not found',
-    error: err.message,
+    message: 'Route not found',
   });
 };
