2a0205086aa41765c70111a2a18bd3c066a34f18
KateMassa <catherineorlova27@gmail.com>
2024-05-27 15:07:43 +0200

    reset last try

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,14 +1,14 @@
-// import { HttpError } from 'http-errors';
+import { HttpError } from 'http-errors';
 
 export const errorHandler = (err, req, res) => {
-  // if (err instanceof HttpError) {
-  //   res.status(err.status).json({
-  //     status: err.status,
-  //     message: err.name,
-  //     data: err,
-  //   });
-  //   return;
-  // }
+  if (err instanceof HttpError) {
+    res.status(err.status).json({
+      status: err.status,
+      message: err.name,
+      data: err,
+    });
+    return;
+  }
 
   res.status(500).json({
     status: 500,
