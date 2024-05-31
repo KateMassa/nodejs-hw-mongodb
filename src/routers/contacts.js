a9cb852fa56212da22351abb8799bdba7637eb71
KateMassa <catherineorlova27@gmail.com>
2024-05-31 15:21:58 +0200

    add midware auth

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -16,8 +16,11 @@ import {
   updateContactSchema,
 } from '../validation/contacts.js';
 import { validateBody } from '../middlewares/validateBody.js';
+import { authenticate } from '../middlewares/authenticate.js';
 
 const router = Router();
+
+router.use(authenticate);
 
 router.get('/', ctrlWrapper(getContactsController));
 
