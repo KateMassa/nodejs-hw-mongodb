eaa5bf1baff0772c9add32eef991b0ab9c774e04
KateMassa <catherineorlova27@gmail.com>
2024-06-03 09:52:34 +0200

    ++

--- src/validation/auth.js
+++ src/validation/auth.js
@@ -1,11 +1,15 @@
 import Joi from 'joi';
 
 export const registerUserSchema = Joi.object({
-  name: Joi.string().min(3).max(30).required(),
+  name: Joi.string().min(3).max(30).required().messages({
+    'string.empty': 'Set name for user',
+  }),
   email: Joi.string().email().required().messages({
     'string.empty': 'Set email for user',
   }),
-  password: Joi.string().required(),
+  password: Joi.string().required().messages({
+    'string.empty': 'Set password for user',
+  }),
 });
 
 export const loginUserSchema = Joi.object({
