333d25b5f90b7cd758d483cecc9d5569a18d4fdb
KateMassa <catherineorlova27@gmail.com>
2024-06-13 14:56:14 +0200

    Update contacts.js

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -22,25 +22,32 @@ const router = Router();
 
 router.use(authenticate);
 
-router.get('/', ctrlWrapper(getContactsController));
+router.get('/', authenticate, ctrlWrapper(getContactsController));
 router.get('/:contactId', ctrlWrapper(getContactByIdController));
 
 router.post(
   '/',
+  authenticate,
   validateBody(createContactSchema),
   ctrlWrapper(createContactController),
 );
 
-router.delete('/:contactId', ctrlWrapper(deleteContactController));
+router.delete(
+  '/:contactId',
+  authenticate,
+  ctrlWrapper(deleteContactController),
+);
 
 router.put(
   '/:contactId',
+  authenticate,
   validateBody(updateContactSchema),
   ctrlWrapper(updateContactController),
 );
 
 router.patch(
   '/:contactId',
+  authenticate,
   validateBody(updateContactSchema),
   ctrlWrapper(patchContactController),
 );
